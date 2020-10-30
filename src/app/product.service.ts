1b7bbc615c8a4b0830bcf84a2d915dea800c6d84
alakumk <alakumk@IT004>
2020-10-30 12:17:48 +0100

    abc

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  private _albumUrl = '../asstes/album.json';
+  _albumUrl = '../asstes/album.json';
 
   constructor(private _http: Http) { }
 
